0f637641143f52674065f48121af231cdd43d335
Stefan Friesel <sf@cloudcontrol.de>
2015-06-15 11:36:36 +0200

    Remove deprecated patterns()

--- test_urls.py
+++ test_urls.py
@@ -2,5 +2,4 @@ from django.conf.urls import patterns, url, include
 from django.contrib import admin
 admin.autodiscover()
 
-urlpatterns = patterns("",
-        url(r'^admin/', include(admin.site.urls)))
+urlpatterns = [url(r'^admin/', include(admin.site.urls))]
